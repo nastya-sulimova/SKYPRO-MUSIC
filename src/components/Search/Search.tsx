d323eda1b3d190259400dd81df86d8973dcb3aca
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2026-01-05 22:15:45 +0300

    созданы файлы helper.ts (функции для форматирования продолжительности трека и фильтрации уникальных позиций), sharedTypes.ts (типизация моковых данных), data.ts с моковыми данными песен, выделен в отдельный компонент блок Search

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './search.module.css';
+
+type searchProp = {
+  title: string;
+};
+
+export default function Search({ title }: searchProp) {
+  const [searchInput, setSearchInput] = useState('');
+
+  const onSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <div className={styles.centerblock__search}>
+      <svg className={styles.search__svg}>
+        <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
+      </svg>
+      {title}
+      <input
+        className={styles.search__text}
+        type="search"
+        placeholder="Поиск"
+        name="search"
+        value={searchInput}
+        onChange={onSearchInput}
+      />
+    </div>
+  );
+}
