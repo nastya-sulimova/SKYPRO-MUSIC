5e3fb63812d69208669b4c55d094603cdb58d2fc
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2026-01-05 23:46:02 +0300

    реализовано открытие/закрытие меню при нажатии на иконку бургера

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './navigation.module.css';
 
 export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const handleBurgerClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.main__nav}>
       <div className={styles.nav__logo}>
@@ -14,12 +25,14 @@ export default function Navigation() {
           alt={'logo'}
         />
       </div>
-      <div className={styles.nav__burger}>
+      <div onClick={handleBurgerClick} className={styles.nav__burger}>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
       </div>
-      <div className={styles.nav__menu}>
+      <div
+        className={`${styles.nav__menu} ${isMenuOpen ? styles.menu_open : ''}`}
+      >
         <ul className={styles.menu__list}>
           <li className={styles.menu__item}>
             <Link href="#" className={styles.menu__link}>
