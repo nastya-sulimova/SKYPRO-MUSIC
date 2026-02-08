2d0d6ffc956a9ce04fb0668e61971a132d17af67
nastya-sulimova <nastya.sulimova.psk@gmail.com>
2026-02-08 16:21:07 +0300

    удалены лишние файлы и закомментированный код

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,3 @@
-// src/store/store.ts
-
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {
   TypedUseSelectorHook,
@@ -19,20 +17,12 @@ export const makeStore = () => {
   });
 };
 
-// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 type RootState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore['dispatch'];
 
-// Для нового TS
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
 
-// // Для старого TS
-// export const useAppDispatch: () => AppDispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-// export const useAppStore: () => AppStore = useStore;
